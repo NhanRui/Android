ae7f8479e220bd719474b0dd6bea946b84e02ed9
Le Hoang Phuc <phucyugi1@gmail.com>
2021-03-05 23:02:50 +0700

    project default

---
+++ gallery_noob/app/src/main/java/com/example/gallery_noob/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.gallery_noob;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment3);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
